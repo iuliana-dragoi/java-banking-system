4e32933fdf6cbf522564b6e7e4c252af02ffa3dd
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-05-06 20:00:55 +0300

    Implement simple Load balancer.

---
+++ src/main/java/LoadBalancer/Tests/Client.java
@@ -0,0 +1,41 @@
+package main.java.LoadBalancer.Tests;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+        new Client().run();
+    }
+
+    private void run() {
+
+        try{
+            InetAddress host = InetAddress.getLocalHost();
+            System.out.println(host + " " + host.getHostAddress());
+            Socket socket = new Socket(host.getHostName(), 8080);
+
+            sendMessageToServer(socket);
+            getMessageFromServer(socket);
+
+        }
+        catch (IOException | RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void sendMessageToServer(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(outputStream, true);
+        writer.println("Hello from Client");
+    }
+
+    private static void getMessageFromServer(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String message = reader.readLine();
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/LoadBalancer/Tests/Server.java
@@ -0,0 +1,55 @@
+package main.java.LoadBalancer.Tests;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+        new Server().run();
+    }
+
+    private void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            System.out.println("Backend server running on port " + 8080);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(() -> handleClient(clientSocket)).start();
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try {
+            System.out.println("Client " + clientSocket.getRemoteSocketAddress());
+            getMessageFromClient(clientSocket);
+            sendMessageToClient(clientSocket);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getMessageFromClient(Socket clientSocket) throws IOException {
+        InputStream inputStream = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String message = reader.readLine();
+        System.out.println("Server received: " + message);
+    }
+
+    private void sendMessageToClient(Socket clientSocket) throws IOException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        PrintWriter writer = new PrintWriter(outputStream, true);
+        writer.println("Hello from Server");
+    }
+}
